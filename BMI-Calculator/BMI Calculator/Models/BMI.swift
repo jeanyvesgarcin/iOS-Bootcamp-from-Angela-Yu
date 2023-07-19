e2f5fdde2dec175bf659d873a23a2f2ca813aa83
Jean-Yves GARCIN <114491721+jeanyvesgarcin@users.noreply.github.com>
2023-07-19 11:36:52 +0200

    Add files via upload

---
+++ BMI-Calculator/BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Jean-Yves Garcin.
+//
+
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
