67efc7be47f064a89f4f02e22e6eaa958da0ba52
Jean-Yves GARCIN <114491721+jeanyvesgarcin@users.noreply.github.com>
2023-07-19 11:54:39 +0200

    Add files via upload

---
+++ Quizzler/Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Jean-Yves Garcin.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
+
+    
