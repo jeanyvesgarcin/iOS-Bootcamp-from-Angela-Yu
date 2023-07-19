df6e54a0f934e75171d454816fc16b4a628a8f50
Jean-Yves GARCIN <114491721+jeanyvesgarcin@users.noreply.github.com>
2023-07-19 11:42:39 +0200

    Add files via upload

---
+++ Destini/Destini-iOS13/Model/Story.swift
@@ -0,0 +1,17 @@
+//
+//  Sotry.swift
+//  Destini-iOS13
+//
+//  Created by Jean-Yves Garcin.
+//
+
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
