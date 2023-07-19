c7225f6f164bf69a374c50e188bcf470c15d2945
Jean-Yves GARCIN <114491721+jeanyvesgarcin@users.noreply.github.com>
2023-07-19 11:50:48 +0200

    Add files via upload

---
+++ I am Rich 2/I am Rich 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich 2
+//
+//  Created by Jean-Yves Garcin.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
